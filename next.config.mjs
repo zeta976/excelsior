2635ad711fa15e6245c1b1e7e8a05bacf89ebcb1
unknown <alejandrozuluagarincon2007@gmail.com>
2025-05-16 19:10:06 -0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/admin/config.yml',
+        destination: '/admin/config.yml',
+      },
+      {
+        source: '/admin',
+        destination: '/admin/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
